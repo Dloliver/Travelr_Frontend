e34c04fefccf84f303f8000ba6a5b64429ee7d48
robwhite <robert.white3120@gmail.com>
2021-11-18 19:23:36 -0600

    created log in and sign up toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,32 +2,47 @@ import React, { useState } from 'react'
 import '../css/header.css'
 
 const Header = (params) => {
+     const [signUpHidden, setSignUpHidden] = useState(true)
+     const [logInHidden, setLogInHidden] = useState(true)
+
+     const handleButtonToggle = (e) => {
+          if(e.target.id == "sign-up"){
+               setLogInHidden(true)
+               setSignUpHidden(false)
+          }else{
+               setLogInHidden(false)
+               setSignUpHidden(true)
+          }
+     }
+     
      return (
           <>
           <div className="header">
                <h1>Travelr</h1>
 
                <div className="button-container">
-                    <button>Create Account</button>
-                    <button>Log In</button>
+                    <button id="sign-up" onClick={handleButtonToggle}>Sign Up</button>
+                    <button id="log-in" onClick={handleButtonToggle}>Log In</button>
                </div>
           </div>
 
-          <div className="create-account hidden">
+          <div className={signUpHidden ? "hidden" : "create-account"}>
                <form className="auth">
                     <label>Username: </label>
                     <input type="text" name="email" />
                     <label>Password: </label>
-                    <input type="password" name="password" /> 
+                    <input type="password" name="password" /><br />
+                    <input type="submit" value="Create Account"/> 
                </form>
           </div>
           
-          <div className="login">
+          <div className={logInHidden ? "hidden" : "login"}>
                <form className="auth">
                     <label>Username: </label>
                     <input type="text" name="email" />
                     <label>Password: </label>
-                    <input type="password" name="password" /> 
+                    <input type="password" name="password" /><br />
+                    <input type="submit" value="Log in"/> 
                </form>
           </div>
           </>
